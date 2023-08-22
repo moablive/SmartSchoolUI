89ecdc861f61df1c430816398becd59034d6a013
Moab <guilhermeferrazbonato@gmail.com>
2023-08-22 11:08:17 -0300

    import Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,41 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from "ngx-spinner";
 
+//Alunos
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { ProfessoresAlunosComponent } from './components/alunos/professores-alunos/professores-alunos.component';
+
+//Dashboard
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+//Perfil
+import { PerfilComponent } from './components/perfil/perfil.component';
+
+
+//Professores
+import { ProfessoresComponent } from './components/professores/professores.component'
+import { AlunosProfessoresComponent } from './components/professores/alunos-professores/alunos-professores.component';
+import { ProfessorDetalheComponent } from './components/professores/professor-detalhe/professor-detalhe.component';
+
+
+//NAV
+import { NavComponent } from './components/shared/nav/nav.component';
+import { TituloComponent } from './components/shared/titulo/titulo.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
+    ProfessoresAlunosComponent,
+    DashboardComponent,
+    PerfilComponent,
+    ProfessoresComponent,
+    AlunosProfessoresComponent,
+    ProfessorDetalheComponent,
+    NavComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule,
