01de01a3d630a4063184d8dd41dbe83121153d14
Moab <guilhermeferrazbonato@gmail.com>
2023-08-22 10:46:39 -0300

    components

---
+++ src/app/components/alunos/professores-alunos/professores-alunos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professores-alunos',
+  templateUrl: './professores-alunos.component.html',
+  styleUrls: ['./professores-alunos.component.css']
+})
+export class ProfessoresAlunosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/professores/alunos-professores/alunos-professores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alunos-professores',
+  templateUrl: './alunos-professores.component.html',
+  styleUrls: ['./alunos-professores.component.css']
+})
+export class AlunosProfessoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
